7598aba6b6b03a86de6ce003f4fb57d155cc174a
Bart Verbruggen <bart@statik.be>
2016-10-17 09:10:57 +0200

    fix watch file

--- gulpfile.js/tasks/watch.js
+++ gulpfile.js/tasks/watch.js
@@ -4,19 +4,17 @@ var path = require('path');
 var watch = require('gulp-watch');
 
 var watchTask = function() {
-  var watchableTasks = ['fonts', 'images', 'svgSprite', 'html', 'css'];
+  var watchableTasks = ['fonts', 'images', 'svgSprite','html', 'css'];
 
-  for (var i = 0, len = watchableTasks.length; i < len; i++) {
-    var taskName = watchableTasks[0];
+  watchableTasks.forEach(function(taskName) {
     var task = config.tasks[taskName];
-
     if(task) {
       var glob = path.join(config.root.src, task.src, '**/*.{' + task.extensions.join(',') + '}');
       watch(glob, function() {
        require('./' + taskName)();
       });
     }
-  }
+  });
 };
 
 gulp.task('watch', ['browserSync'], watchTask);
